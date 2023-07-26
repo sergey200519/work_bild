971fe3d9371e8046ba5f9649ca1f45ca42417dd2
sergey200519 <63727105+sergey200519@users.noreply.github.com>
2023-07-26 19:55:15 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 import { newSwipper } from "./swipper.js";
 import { newSteps } from "./steps.js";
+import { newAccordion } from "./accordion.js";
 
 newSwipper(".swipper")
 newSteps(".work")
+newAccordion(".questions_box")
